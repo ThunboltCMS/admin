8a4659b30e454ae093e916609430d7e1f8f7af22
MartkCz <antik494@gmail.com>
2018-07-16 15:56:33 +0200

    custom.scss

--- src/AdminBundle/Presenters/HomepagePresenter.php
+++ src/AdminBundle/Presenters/HomepagePresenter.php
@@ -3,7 +3,6 @@
 namespace Thunbolt\Administration\AdminBundle\Presenters;
 
 use Thunbolt\Administration\AdminPresenter;
-use Ublaboo\DataGrid\DataGrid;
 
 class HomepagePresenter extends AdminPresenter {
 
@@ -12,36 +11,6 @@ class HomepagePresenter extends AdminPresenter {
 		$list[] = __DIR__ . '/../templates/@layout.latte';
 
 		return $list;
-	}
-
-	protected function createComponentTestGrid() {
-		$grid = new DataGrid();
-
-		$grid->setDataSource([
-			['id' => 1, 'name' => 'Foo 1', 'desc' => 'Bar 1'],
-			['id' => 2, 'name' => 'Foo 2', 'desc' => 'Bar 2'],
-			['id' => 2, 'name' => 'Foo 2', 'desc' => 'Bar 2'],
-			['id' => 2, 'name' => 'Foo 2', 'desc' => 'Bar 2'],
-			['id' => 2, 'name' => 'Foo 2', 'desc' => 'Bar 2'],
-			['id' => 2, 'name' => 'Foo 2', 'desc' => 'Bar 2'],
-			['id' => 2, 'name' => 'Foo 2', 'desc' => 'Bar 2'],
-			['id' => 2, 'name' => 'Foo 2', 'desc' => 'Bar 2'],
-			['id' => 2, 'name' => 'Foo 2', 'desc' => 'Bar 2'],
-			['id' => 2, 'name' => 'Foo 2', 'desc' => 'Bar 2'],
-			['id' => 2, 'name' => 'Foo 2', 'desc' => 'Bar 2'],
-			['id' => 2, 'name' => 'Foo 2', 'desc' => 'Bar 2'],
-			['id' => 2, 'name' => 'Foo 2', 'desc' => 'Bar 2'],
-		]);
-
-		$grid->addColumnText('name', 'Foo')
-			->setSortable()
-			->setFilterText();
-		$grid->addColumnText('desc', 'Bar');
-		$grid->addActionCallback('delete', 'Delete', function () {
-			bdump('xxx');
-		});
-
-		return $grid;
 	}
 
 	public function renderDefault(): void {
